5e2448ae66a5a081dd5b82a2c83b658467b62252
matite <matiastite1@gmail.com>
2024-01-29 17:15:41 -0500

    Example of how to use loadingBuilder to show a message while an image charge

--- lib/presentation/widgets/chat/other_message_bubble.dart
+++ lib/presentation/widgets/chat/other_message_bubble.dart
@@ -44,6 +44,16 @@ class _ImageBubble extends StatelessWidget {
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+
+            return Container(
+                width: size.width * 0.7,
+                height: 150,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Text("Scoby esta pensando..."));
+          },
         ),
       ),
     );
