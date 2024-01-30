cd09cbfe616e8127db4bfcbbea56689fe73c0c20
matite <matiastite1@gmail.com>
2024-01-30 12:33:13 -0500

    Changed _ChatView widget to use the ChatProvider messages

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> mesages = [
+  List<Message> messageList = [
     Message(text: "Hobis hije", fromWho: FromWho.me),
     Message(text: "Ya comiste?", fromWho: FromWho.me)
   ];
